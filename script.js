cf722981f19785c6c6658b33f3ef255d08b4a255
Hale Muir <halemuir@Hales-Air.lan>
2024-05-18 13:59:40 -0500

    Added Local Storage and other css to make my website look more complete. I still need to fix my local storage on the switch text color but I am hoping I can ask this question in class before we share our projects.

--- script.js
+++ script.js
@@ -5,6 +5,20 @@ let charecterText;
 let missTypes = 0;
 let timer;
 
+async function getUsersName(){
+    let userName = document.querySelector(`#userName`);
+    
+    let name = null;
+    if(localStorage.getItem(`name`) !== null) {
+        name = localStorage.getItem(`name`);
+    }
+    else{
+        name = prompt(`What is your name?`);
+    }
+    userName.textContent = name;
+    localStorage.setItem(`name`, name);
+}
+
 async function getQuote(){
     const api_url ="https://zenquotes.io/api/quotes/";
     let data = null
@@ -38,6 +52,14 @@ function displayQuote(data){
     }
 
     document.addEventListener('keyup', typing)
+    let switchColorBtn = document.querySelector('#switchTextColor');
+    switchColorBtn.addEventListener('click', switchTextColor);
+    let reloadBtn = document.querySelector('#reload');
+    reloadBtn.addEventListener('click', reloadSite);
+}
+
+function reloadSite(){
+    location.reload();
 }
 
 function typing(event){
@@ -56,7 +78,6 @@ function typing(event){
         }
     }
     if (typingCorrect){
-        // typing.style.background = 'rgb(52, 52, 52)'
         typing.style.background = 'transparent'
     }
     else{
@@ -93,8 +114,130 @@ function wordsPerMinuteCalc(elapsedTime){
     WPMText.textContent = `Words Per Minute: ` + WPM;
 }
 
+function switchTextColor(){
+    // let switchColorBtn = document.querySelector('#switchTextColor');
+    console.log('switchTextColor is being run!')
+
+    const typing = document.querySelector('#typing'); 
+    const p1 = document.querySelector('#quote');
+    const p2 = document.querySelector('#author');
+    const p3 = document.querySelector('#charecters');
+    const p4 = document.querySelector('#WPM');
+    const p5 = document.querySelector('#userName');
+    const p6 = document.querySelector('#title');
+    const p7 = document.querySelector('#clear-local-storage');
+    const p8 = document.querySelector('#switchTextColor');
+    const p9 = document.querySelector('#reload');
+
+    localStorage.getItem('typing');
+    localStorage.getItem('p1');
+    localStorage.getItem('p2');
+    localStorage.getItem('p3');
+    localStorage.getItem('p4');
+    localStorage.getItem('p5');
+    localStorage.getItem('p6');
+    localStorage.getItem('p7');
+    localStorage.getItem('p8');
+    localStorage.getItem('p9');
+
+    if(typing.style.color === 'white'){
+        typing.style.color = 'black';
+        typing.style.borderColor = 'black';
+        p1.style.color = 'black';
+        p2.style.color = 'black';
+        p3.style.color = 'black';
+        p4.style.color = 'black';
+        p5.style.color = 'black';
+        p6.style.color = 'black';
+        p7.style.color = 'black';
+        p7.style.borderColor = 'black';
+        p8.style.color = 'black';
+        p8.style.borderColor = 'black';
+        p9.style.color = 'black';
+        p9.style.borderColor = 'black';
+    }
+    else{
+        typing.style.color = 'white';
+        typing.style.borderColor = 'white';
+        p1.style.color = 'white';
+        p2.style.color = 'white';
+        p3.style.color = 'white';
+        p4.style.color = 'white';
+        p5.style.color = 'white';
+        p6.style.color = 'white';
+        p7.style.color = 'white';
+        p7.style.borderColor = 'white';
+        p8.style.color = 'white';
+        p8.style.borderColor = 'white';
+        p9.style.color = 'white';
+        p9.style.borderColor = 'white';
+    }
+}
+
+function updateColor() {
+    //getting the value of each slider
+    const redSlider  = document.querySelector(`#red`)
+    const red = redSlider.value;
+    const greenSlider = document.querySelector(`#green`);
+    const green = greenSlider.value;
+    const blueSlider = document.querySelector(`#blue`);
+    const blue = blueSlider.value;
+
+    const body = document.querySelector(`body`);
+    //changing the background color of the body using the values of the sliders
+    body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+
+    localStorage.setItem(`red`, red);
+    localStorage.setItem(`blue`, blue);
+    localStorage.setItem(`green`, green);
+}
+
+function makeColorSlidersWork() {
+    //getting the <input> elements (a.k.a sliders)
+    const redSlider = document.querySelector(`#red`);
+    const greenSlider = document.querySelector(`#green`);
+    const blueSlider = document.querySelector(`#blue`);
+
+    let sliders = [redSlider, greenSlider, blueSlider];
+    //adding the same event listener to all sliders
+    for(let slider of sliders) {
+        slider.addEventListener(`input`, updateColor);
+    }
+
+    const red = localStorage.getItem(`red`);
+    const blue = localStorage.getItem(`blue`);
+    const green = localStorage.getItem(`green`);
+    
+    if(red !== null){
+        redSlider.value = red;
+    }
+    if(blue !== null){
+        blueSlider.value = blue;
+    }
+    if(green !== null){
+        greenSlider.value = green;
+    }
+    if(red !== null && blue !== null && green !== null){
+        const body = document.querySelector(`body`);
+        body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    }
+}
+
+function clearLocalStorage() {
+    localStorage.clear();
+    console.log(`Local storage cleared`);
+    console.log(localStorage);
+}
+function makeLocalStorageWork() {
+    let localStorageButton = document.querySelector(`#clear-local-storage`);
+    localStorageButton.addEventListener(`click`, clearLocalStorage);
+}
+
 function runProgram(){
+    getUsersName()
     console.log('runProgram');
     getQuote()
+    makeColorSlidersWork();
+    makeLocalStorageWork();
 }
 document.addEventListener('DOMContentLoaded', runProgram);
